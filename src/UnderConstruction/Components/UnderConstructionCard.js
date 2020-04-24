af0ffb22281396934067b62c0a64b16cc31bbd5f
Felix Arribas Pardo <felix.arribas@skyscanner.net>
2020-04-24 16:57:55 +0200

    Change logo and background alpha

--- src/UnderConstruction/Components/UnderConstructionCard.js
+++ src/UnderConstruction/Components/UnderConstructionCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import ListGroup from "react-bootstrap/ListGroup";
+import aBlank from "../../Utils/aBlank";
 import "../index.css";
 
 const Styles = {
   container: {
     marginTop: '2em',
     marginBottom: '2em',
-    backgroundColor: 'white',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
   },
   image: {
     paddingLeft: '1em',
@@ -21,19 +22,19 @@ const UnderConstructionCard = () => (
     <Card.Body>
       <Card.Text><i>Pàgina web en construcció...</i></Card.Text>
     </Card.Body>
-    <Card.Img variant="top" src="logob.jpg" style={Styles.image} />
+    <Card.Img variant="top" src="logo_v2.png" style={Styles.image} />
     <Card.Body>
       <Card.Subtitle>Contacta amb nosaltres</Card.Subtitle>
     </Card.Body>
     <ListGroup className="list-group-flush text-justify-left">
       <ListGroup.Item>
-        <a href={"tel:+34936745559"} target={"_blank"}>(+34) 936 745 559</a>
+        <aBlank href={"tel:+34936745559"}>(+34) 936 745 559</aBlank>
       </ListGroup.Item>
       <ListGroup.Item>
-        <a href={"https://goo.gl/maps/jQkJF2Bpe4Ek4qUw7"} target={"_blank"}>Dr. Murillo 3 · 08172 St. Cugat · Barcelona</a>
+        <aBlank href={"https://goo.gl/maps/jQkJF2Bpe4Ek4qUw7"}>Dr. Murillo 3 · 08172 St. Cugat · Barcelona</aBlank>
       </ListGroup.Item>
       <ListGroup.Item>
-        <a href={"mailto:jbalanya@quadrant12.com"} target={"_blank"}>jbalanya@quadrant12.com</a>
+        <aBlank href={"mailto:derzu@quadrant12.com"}>derzu@quadrant12.com</aBlank>
       </ListGroup.Item>
     </ListGroup>
   </Card>
