7d7a3c3f76169aaa2c80f04a66457f78c8a82a57
Felix Arribas Pardo <felix.arribas@skyscanner.net>
2020-04-27 17:14:26 +0200

    fix 2nd address text lines

--- src/UnderConstruction/Components/UnderConstructionCard.js
+++ src/UnderConstruction/Components/UnderConstructionCard.js
@@ -8,7 +8,7 @@ const Styles = {
   container: {
     marginTop: '2em',
     marginBottom: '2em',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    backgroundColor: 'rgba(255, 255, 255, 0.85)',
   },
   image: {
     paddingLeft: '1em',
@@ -22,7 +22,7 @@ const contactLinks = [
   { url: 'tel:+34936745559', text: '(+34) 936 745 559' },
   { url: 'mailto:derzu@quadrant12.com', text: 'derzu@quadrant12.com' },
   { url: 'https://goo.gl/maps/jQkJF2Bpe4Ek4qUw7', text: 'Dr. Murillo 3 · 08172 St. Cugat · Barcelona' },
-  { url: 'https://goo.gl/maps/vrMhWqGXxfdfkhdj9', text: 'Carrer Major 64, 3r · 25100 Almacelles · Lleida' },
+  { url: 'https://goo.gl/maps/vrMhWqGXxfdfkhdj9', text: 'Major 64, 3r · 25100 Almacelles · Lleida' },
 ];
 
 const UnderConstructionCard = () => (
