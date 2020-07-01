7ddae7ab2ee527adfb90567982b4d2a54cce7d87
Felix Arribas Pardo <felix.arribas@skyscanner.net>
2020-07-01 11:45:55 +0200

    fix image aspect ratio

--- src/Pages/Projects/Components/ProjectCard.js
+++ src/Pages/Projects/Components/ProjectCard.js
@@ -4,7 +4,9 @@ import "../index.scss";
 
 const ProjectCard = (props) => (
   <Card className="project-card">
-    <Card.Img variant="top" src={props.imageURL} />
+    <div className="card-img-container">
+      <img className="card-img" src={props.imageURL} />
+    </div>
     <Card.Body>
       <Card.Title>{props.title}</Card.Title>
       <Card.Text><Badge variant="secondary">{props.location}</Badge>{` ${props.description.substring(0, 140)}...`}</Card.Text>
