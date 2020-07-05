e4945252974e8d86e08d450b647c3f04511c1981
Felix Arribas Pardo <felix.arribas@skyscanner.net>
2020-07-05 17:39:19 +0200

    refactor Projects Item and Card style

--- src/Pages/Projects/Components/ProjectCard.js
+++ src/Pages/Projects/Components/ProjectCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import "../index.scss";
 
 const ProjectCard = (props) => (
-  <div className="project-card-container">
+  <div className="project-card-container project-clickable-element">
     <div className="project-card-img-container">
-      <img className="project-card-img" src={props.imageURL}  alt={`Project: ${props.title}`}/>
+      <img className="project-img project-card-img" src={props.imageURL}  alt={`Project: ${props.title}`}/>
     </div>
     <div className="project-card-content">
       <h5>{props.title}</h5>
-      <p><div className="project-card-badge">{props.location}</div>{` ${props.description.substring(0, 140)}...`}</p>
+      <p><span className="project-badge">{props.location}</span>{` ${props.description.substring(0, 140)}...`}</p>
     </div>
   </div>
 );
