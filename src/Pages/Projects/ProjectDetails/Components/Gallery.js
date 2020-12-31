b461b161bc533d0af459a656f4f4d07fc3c5fb69
Felix Arribas Pardo <felix.arribas@skyscanner.net>
2020-12-31 10:27:59 +0100

    Refacto carousel

--- src/Pages/Projects/ProjectDetails/Components/Gallery.js
+++ src/Pages/Projects/ProjectDetails/Components/Gallery.js
@@ -1,6 +1,18 @@
 import { Carousel, Container, Row } from "react-bootstrap";
 import React from "react";
 
+const buildItems = (images, projectId) =>
+  images.map(imageName => (
+    <Carousel.Item key={imageName}>
+      <img
+        alt="Project"
+        className="d-block w-100"
+        src={`/images/${projectId}/${imageName}`}
+      />
+    </Carousel.Item>
+  )
+);
+
 const Gallery = (props) => (
   <Container className="project-details-gallery-container">
     <Row className="project-details-section">
@@ -10,15 +22,7 @@ const Gallery = (props) => (
       <Carousel
         className="project-details-carousel"
         inteval={2000}>
-        {props.images.map(imageName => (
-          <Carousel.Item key={imageName}>
-            <img
-              alt="Project"
-              className="d-block w-100"
-              src={`/images/${props.projectId}/${imageName}`}
-            />
-          </Carousel.Item>
-        ))}
+        {buildItems(props.images, props.projectId)}
       </Carousel>
     </Row>
   </Container>
